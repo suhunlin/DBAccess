79e90aba26b6516d3a06faf387fbe68496b2c6eb
suhunlin <lin.suhun@msa.hinet.net>
2023-07-20 19:00:42 +0800

    1.Add MyDBHelper extends SQLiteOpenHelper class 2.Initial and create database

--- app/src/main/java/com/suhun/dbaccess/MainActivity.java
+++ app/src/main/java/com/suhun/dbaccess/MainActivity.java
@@ -2,13 +2,18 @@ package com.suhun.dbaccess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase db;
+    private MyDBHelper myDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDBHelper = new MyDBHelper(this, "happyDB", null, 1);
+        db = myDBHelper.getWritableDatabase();
     }
 }
---
+++ app/src/main/java/com/suhun/dbaccess/MyDBHelper.java
@@ -0,0 +1,25 @@
+package com.suhun.dbaccess;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private final String createTable = "CREATE TABLE cust (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "cname TEXT, tel TEXT, birthday DATE)";
+    public MyDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
