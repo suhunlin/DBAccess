5bbbc0d16d1bd94ad5dc4af78d6094b57e64b2ad
suhunlin <lin.suhun@msa.hinet.net>
2023-07-20 19:15:57 +0800

    Add and initial layout

--- app/src/main/java/com/suhun/dbaccess/MainActivity.java
+++ app/src/main/java/com/suhun/dbaccess/MainActivity.java
@@ -4,16 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase db;
     private MyDBHelper myDBHelper;
+    private TextView resultDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultDB = findViewById(R.id.result);
         myDBHelper = new MyDBHelper(this, "happyDB", null, 1);
         db = myDBHelper.getWritableDatabase();
+    }
+
+    public void quaryFun(View view){
+
+    }
+    public void insertFun(View view){
+
+    }
+    public void deleteFun(View view){
+
+    }
+    public void updateFun(View view){
+
     }
 }
